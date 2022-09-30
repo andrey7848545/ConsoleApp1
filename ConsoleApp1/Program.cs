baf2f0cb8c882884ea135ad763415383944004f0
andre <andre@DESKTOP-9RC7R2U>
2022-09-30 20:04:26 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("старт");
+    Console.WriteLine("введите x");
+    double x = double.Parse(Console.ReadLine());
+    double res = (x * x - 7 * x) / (x * x - 8 * x + 12);
+    Console.WriteLine(res);
+    Console.WriteLine("конец");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());   
+}
